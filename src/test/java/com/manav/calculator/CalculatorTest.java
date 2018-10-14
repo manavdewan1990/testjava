2b452ffef535816497edb7f5dedf8a7a2e6fe497
manavd <manavd@hotstar.com>
2018-10-14 10:36:00 +0530

    test updated

--- src/test/java/com/manav/calculator/CalculatorTest.java
+++ src/test/java/com/manav/calculator/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CalculatorTest {
+class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
